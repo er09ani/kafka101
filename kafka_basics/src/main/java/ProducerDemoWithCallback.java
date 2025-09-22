d681c36f121f75582ca461b507da25206628cc0c
er09ani <er09ani@gmail.com>
2025-09-22 23:48:34 +0530

    add producer record with key

--- kafka_basics/src/main/java/ProducerDemoWithCallback.java
+++ kafka_basics/src/main/java/ProducerDemoWithCallback.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.Random;
+import java.util.random.RandomGenerator;
 
 public class ProducerDemoWithCallback {
     private static final Logger log = LoggerFactory.getLogger(ProducerDemoWithCallback.class.getSimpleName());
@@ -13,16 +15,16 @@ public class ProducerDemoWithCallback {
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        RandomGenerator randomGenerator = new Random();
+        randomGenerator.ints(1);
+
         try(Producer<String, String> producer = new KafkaProducer<>(props)) {
-            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "first message");
-            producer.send(record, new Callback() {
-                @Override
-                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
-                    if(e == null) {
-                        log.info("producing the message {} {}  {}",  recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
-                    }else {
-                        log.error("error while producing the message");
-                    }
+            ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", String.valueOf(new Random().nextInt()), "first message");
+            producer.send(record, (recordMetadata, e) -> {
+                if(e == null) {
+                    log.info("producing the message {} : {} : {} : {}",  recordMetadata.topic(), record.key(), recordMetadata.partition(), recordMetadata.offset());
+                }else {
+                    log.error("error while producing the message");
                 }
             });
         }
